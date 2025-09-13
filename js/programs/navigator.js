5cf491f60bab5ec8dbeb114db9a9d45f9e327eef
almasso <almasso@ucm.es>
2025-09-13 21:43:31 +0200

    Navegador ahora sí

---
+++ js/programs/navigator.js
@@ -0,0 +1,129 @@
+import Program from "../program.js";
+import {getRoot} from "../utils.js";
+
+
+export default class Navigator extends Program {
+
+    static icon = "navigator.png";
+    static id = "navigator";
+    static name = "Navigator";
+    static unique = false;
+
+    constructor(os) {
+        super(os, Navigator.name, Navigator.id, Navigator.icon, "desktop");
+
+        this.container = null;
+        this.addedListeners = false;
+
+        this.historyStack = [];
+        this.currentIndex = -1;
+
+        this.addEventListener("localeSet", (e) => {
+            this.setLanguage(os.locale);
+            this.getProgramData();
+        });
+    }
+
+    async getProgramData() {
+        await this.langReady();
+        const programData = this.searchForProgramInData();
+        if(programData) {
+            this.strings = programData["texts"];
+        } else {
+            console.warn("No data found for program", this.id);
+            this.strings = {};
+        }
+        this.os.dispatchEvent(new CustomEvent("langLoaded", {}));
+    }
+
+    gainedFocus() {
+        const win = document.getElementById(this.instanceID);
+        const navigatorDiv = win.querySelector("#navigator");
+        this.backButton = navigatorDiv.querySelector("#left-buttons button:nth-child(1)");
+        this.forwardButton = navigatorDiv.querySelector("#left-buttons button:nth-child(2)");
+        this.homeButton = navigatorDiv.querySelector("#left-buttons button:nth-child(3)");
+        this.reloadButton = navigatorDiv.querySelector("#right-buttons button:nth-child(1)");
+        this.stopButton = navigatorDiv.querySelector("#right-buttons button:nth-child(2)");
+        this.urlInput = navigatorDiv.querySelector("#url input");
+        this.iframe = navigatorDiv.querySelector("#content iframe");
+        this.statusBar = navigatorDiv.querySelector("#statusbar");
+
+        if(!this.addedListeners) {
+            this.backButton.addEventListener("click", () => {
+                if(this.currentIndex > 0) {
+                    this.currentIndex--;
+                    this.iframe.src = this.historyStack[this.currentIndex];
+                    this.#updateButtons();
+                }
+            });
+
+            this.forwardButton.addEventListener("click", () => {
+                if(this.currentIndex < this.historyStack.length - 1) {
+                    this.currentIndex++;
+                    this.iframe.src = this.historyStack[this.currentIndex];
+                    this.#updateButtons();
+                }
+            });
+
+            this.homeButton.addEventListener("click", () => {
+                this.navigate("http://home.mcom.com/home/welcome.html");
+            })
+
+            this.reloadButton.addEventListener("click", () => {
+                this.iframe.src = this.iframe.src;
+            });
+
+            this.stopButton.addEventListener("click", () => {
+                this.navigate("about:blank");
+            });
+
+            this.urlInput.addEventListener("keypress", (e) => {
+                if(e.key === "Enter") {
+                    this.navigate(this.urlInput.value);
+                }
+            });
+
+            this.iframe.addEventListener("load", () => {
+                this.stopButton.disabled = true;
+                this.#updateButtons();
+            });
+
+            this.addedListeners = true;
+        }
+    }
+
+    async getBodyHTML() {
+        const response = await fetch(`${getRoot()}html/programs/navigator.html`);
+        return await response.text();
+    }
+
+    static getIcons() {
+        return [{route : "desktop", isAlias : true}];
+    }
+
+    getButtons() {
+        return this.strings;
+    }
+
+    navigate(url) {
+        this.iframe.src = url;
+        this.urlInput.value = url;
+        this.statusBar.textContent = "Loading...";
+
+        this.historyStack = this.historyStack.slice(0, this.currentIndex + 1);
+        this.historyStack.push(url);
+        this.currentIndex++;
+        this.stopButton.disabled = false;
+
+        this.#updateButtons();
+    }
+
+    #updateButtons() {
+        this.backButton.disabled = this.currentIndex <= 0;
+        this.backButton.querySelector("img").src = this.backButton.disabled ? `${getRoot()}assets/icons/programs/navigator/back.png` : `${getRoot()}assets/icons/programs/navigator/back_active.png`;
+        this.forwardButton.disabled = this.currentIndex >= this.historyStack.length - 1;
+        this.forwardButton.querySelector("img").src = this.forwardButton.disabled ? `${getRoot()}assets/icons/programs/navigator/forward.png` : `${getRoot()}assets/icons/programs/navigator/forward_active.png`;
+        this.reloadButton.disabled = false;
+        this.stopButton.querySelector("img").src = this.stopButton.disabled ? `${getRoot()}assets/icons/programs/navigator/stop.png` : `${getRoot()}assets/icons/programs/navigator/stop_active.png`;
+    }
+}
